999b11858d00642b29583ccecef09549b4318255
Yash Sojitra <yash.sojitra@bacancy.com>
2026-03-20 00:04:00 +0530

    Wipe all files to start fresh

--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h2>Page not found</h2>
-      <p>The page you requested does not exist.</p>
-    </div>
-  );
-}
